2a024c669c67e6be148150347e9da3664274f53e
prr123 <priemen@gmail.com>
2022-04-23 15:17:27 +0200

    code progress

--- inclLib/inclLib.go
+++ inclLib/inclLib.go
@@ -13,24 +13,63 @@ import (
 
 const BlockSize=4096
 
-func ListInclFiles(srcfil *os.File) (inclFilNames *[]string, err error) {
-	var files []string
+type InclFilTyp struct {
+	Name string
+	Exist bool
+	Start int64
+	End int64
+}
+
+func parseBlock(inBuf []byte, files []InclFilTyp)(err error) {
+
+	return nil
+}
+
+func ListInclFiles(srcfil *os.File) (inclFilNames *[]InclFilTyp, err error) {
+	var files []InclFilTyp
 	var inBuf []byte
 	var i int64
 
 	inBuf = make([]byte, BlockSize)
-	nb, err := srcfil.Stat()
+	filinfo, err := srcfil.Stat()
 	if err != nil {
 		return nil, fmt.Errorf("srcfil.Stat: %v", err)
 	}
 
-	blocks := nb.Size()/BlockSize
-	blockRem := nb.Size() - blocks*BlockSize
+	blocks := filinfo.Size()/BlockSize
+	blockRem := filinfo.Size() - blocks*BlockSize
 
 	for i=0; i< blocks; i++ {
-
+		_, err = srcfil.Seek(i*BlockSize, 0)
+		if err != nil {
+			return nil, fmt.Errorf("srcfil.Seek: %v", err)
+		}
+		nbyt, err := srcfil.Read(inBuf)
+		if err != nil {
+			return nil, fmt.Errorf("srcfil.Read block %d: %v", i, err)
+		}
+		if nbyt != BlockSize {
+			return nil, fmt.Errorf("srcfil.Read block %d num bytes %d < BlockSize: %v", i, nbyt, err)
+		}
+		err = parseBlock(inBuf, files)
+		if err != nil {
+			return nil, fmt.Errorf("parseBlock block %d: %v", i, err)
+		}
 	}
 
-//	os.Read
+	nbyt, err := srcfil.Read(inBuf)
+	if err != nil {
+		return nil, fmt.Errorf("srcfil.Read last block: %v", err)
+	}
+	if nbyt != int(blockRem) {
+		return nil, fmt.Errorf("srcfil.Read last block: num bytes %d < BlockSize: %v", nbyt, err)
+	}
+
+	err = parseBlock(inBuf, files)
+	if err != nil {
+		return nil, fmt.Errorf("parseBlock last block: %v", i, err)
+	}
+
+
 	return &files, nil
 }
